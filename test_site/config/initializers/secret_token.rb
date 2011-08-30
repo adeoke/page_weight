b5bec41ac2350c49a9cbba0cbccd12b22aeb086d
Ade Oke <adesmail100@gmail.com>
2011-08-30 21:46:37 +0100

    removing unnecessary files

--- test_site/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BasicSite::Application.config.secret_token = 'b0f63abec96bdc00f165b9403aca73df6c685f2851d039521c6a4fca3099f4879edfa70d19a191dd1ca3a1d48efe9a0f03c39b3b9be8aa77c5012d2bff2272b9'
